49284a7586e397bcd45d2af0d2686801ed2128f8
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2023-01-08 16:39:22 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './Bitcoin.svg.png';
+import './App.css';
+import Crypto from './Crypto';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Crypto Rate</h1>
+      </header>
+      <Crypto />
+    </div>
+  );
+}
+
+export default App;
